38cf8b23d36d086b0a724fa44e894a92923fd4ad
3173259499 <3173259499@qq.com>
2024-08-05 15:49:07 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -6,7 +6,7 @@ import { connect } from "cloudflare:sockets";
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
 let userID = "d7e33e9d-3434-4363-89db-a7194de1f9a4";
 
-const proxyIPs = ["cloudflare.com"]; //ts.hpc.tw edgetunnel.anycast.eu.org bestproxy.onecf.eu.org cdn-all.xn--b6gac.eu.org cdn.xn--b6gac.eu.org proxy.xxxxxxxx.tk 85vv.cc cloudflare.com
+const proxyIPs = ["cloudflare.com"]; //ts.hpc.tw edgetunnel.anycast.eu.org bestproxy.onecf.eu.org cdn-all.xn--b6gac.eu.org cdn.xn--b6gac.eu.org proxy.xxxxxxxx.tk 85vv.cc cloudflare.com github.com test-ipv6.com 
 const cn_hostnames = [''];
 let CDNIP = 'www.visa.com.sg'
 // http_ip
